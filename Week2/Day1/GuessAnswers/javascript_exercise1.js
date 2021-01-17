04931fd9c02194bf82dfaba795b4b3e7e6c8f6e0
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-01-17 12:36:30 +0100

    Add files via upload

---
+++ Week2/Day1/GuessAnswers/javascript_exercise1.js
@@ -0,0 +1,13 @@
+    5 + "34" // "534"
+    5 - "4" // 1
+    10 % 5 // 0
+    5 % 10 // 5
+    "Java" + "Script" // "JavaScript"
+    " " + " " // "  "
+    " " + 0 // " 0"
+    true + true // 2
+    true + false // 1
+    false + true // 1
+    false - true // -1
+    3 - 4 // -1
+    "Bob" - "bill" // NaN
