8115692fae86957bc7549aa768c486ebde5a5ded
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-01-17 12:53:50 +0100

    Add files via upload

---
+++ Week2/Day1/FavoriteColor/javascript_array.js
@@ -0,0 +1,9 @@
+// Write a simple JavaScript program to join all elements of the following array into a string.
+
+let me = ["my", "favorite", "color", "is", "blue"];
+
+console.log(me.toString());
+
+console.log(me.join());
+
+console.log(me.join('+'));
