5ca7bac9ac496e7233d2af98466902fb935c3640
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-01-18 08:41:12 +0100

    Update javascript_challenge.js

--- Week2/Day1/Daily Challenge/javascript_challenge.js
+++ Week2/Day1/Daily Challenge/javascript_challenge.js
@@ -1,16 +1,16 @@
 let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
 
 fruits.shift(); 
-console.log( fruits );// Remove "Banana" from the array.
+console.log(fruits); // Remove "Banana" from the array.
 
 fruits.sort();
 console.log(fruits); // Sort the array in order.
 
 fruits.push("Kiwi"); // Put “Kiwi” at the end of the array.
 
-fruits.splice(1, 1); // Remove “Apples” from the array. 
+fruits.splice(0, 1); // Remove “Apples” from the array. 
 
 fruits.reverse(); // Reverse the order of the elements in an array.
 
 let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
-console.log(fruits[0][1][0]) //Get fruit "Oranges"
+console.log(moreFruits[1][1][0]) // Get fruit "Oranges"
