466f26cf16365d48b79cdeb7dba8d9ad2040816f
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-01-19 18:11:20 +0100

    Add files via upload

---
+++ Week2/Day3/Exercises XP/javascript_xp.js
@@ -0,0 +1,110 @@
+
+//Exercise 1
+let colors = ["red", "blue", "green", "yellow"];
+for (let i = 0; i < colors.length; i++) {
+    console.log("My #" + (i + 1) + " color is " + colors[i]);
+}
+
+for (let i = 0; i < colors.length; i++) {
+    let colorsNum = i + 1;
+    let colorsNumSuffix;
+    if (colorsNum == 1) {
+        colorsNumSuffix = 'st';
+    } else if (colorsNum == 2) {
+        colorsNumSuffix = 'nd';
+    } else if (colorsNum == 3) {
+        colorsNumSuffix = 'rd';
+    } else {
+        colorsNumSuffix = 'th';
+    }
+    console.log(" My " + colorsNum + colorsNumSuffix + " color is " + colors[i]);
+}
+
+
+// Exercise 2
+
+let people = ["Greg", "Mary", "Devon", "James"]
+
+for(let i = 0; i < people.length; i++) {
+    console.log(people[i]);
+  }
+
+people.shift();
+
+console.log(people[3]);
+people[3] = "Jason";
+
+people.push("Nessim"); 
+
+for (let counter = 0; counter <= people.length; counter++){
+    if(counter == "Mary") {
+        console.log("exit")
+        break;
+}
+}
+
+console.log(people.slice(0, 3));
+
+people.indexOf("Mary");
+
+people.indexOf(people.length-1);
+
+let last = people[3];
+
+
+
+//Exercise 3
+let numbers = prompt("Give me a new number");
+let i = 0;
+
+while(i < 10){
+    i++;
+    console.log(i)
+    prompt("Give me another number");
+	
+}
+alert("Good job!")
+
+
+
+// Exercise 4
+let guestList = {
+    Randy: "Germany",
+    Karla: "France",
+    Wendy: "Japan",
+    Norman: "England",
+    Sam: "Argentina"
+  }
+
+  let name = prompt("What is your name?");
+  let sentence = "Hi! I'm " + guestList + ", and I'm from " + guestList["country"];
+
+  if (i == guestList.length) {
+    console.log(sentence);
+}
+else {
+    console.log(" Hi! I'm a guest. ");
+}
+
+
+
+//Exercise 5
+let family = { firstName: "Henri", lastName: "Azoulay", age: 62 };
+for (let x in family) {
+    console.log(x + ": " + family[x])
+}
+
+
+//Exercise 6
+
+let names = ["Jack", "Philip", "Sarah", "Amanda", "Bernard", "Kyle"]
+
+let sortedNames = names.sort();
+let secondList = " ";
+
+for (const i in sortedNames) {
+    secondList += sortedNames[i][0];
+}
+
+console.log(secondList);
+
