11ab0d3e18f0bd7014e414447549cdb65f0d603a
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-01-20 22:48:19 +0100

    Add files via upload

---
+++ Week2/Day4/Daily Challenge/daily.js
@@ -0,0 +1,63 @@
+
+
+// Ask a user for several words (separated by commas).
+// Push the words into an array.
+// Console.log them, one per line, in a rectangular frame.
+// For example, if the user gives you: 
+// Hello, World, in, a, frame
+// you will transform it to : ["Hello", "World", "in", "a", "frame"]
+
+// let words = prompt(" Give me a few words ");
+//     words = words.split(" ");
+
+// // function frame (){
+// //     for(x = 0; x <= words.length; x++){
+
+// //       stars = (" * ") + words[x] + (" * ")
+// //     }
+    
+// //    console.log(stars)
+
+// // }
+
+
+let sentence = prompt("Give me a few words");
+
+let sentenceArray = sentence.split(" ");
+
+let starRight = " *";
+
+let starLeft = "* ";
+
+let space = " ";
+
+let temp = 0;
+
+
+function frame (){
+
+    for ( let x = 0; x < sentenceArray.length; x++ ) {
+        if ( temp < sentenceArray[x].length ) {
+            temp = sentenceArray[x].length;
+        }
+    }
+    
+    temp +=4; 
+    let star = "*";
+    for ( let y = 0; y < temp; y++ ) {
+        star += "*";
+    }
+
+    console.log(star);
+
+    for ( let x = 0; x < sentenceArray.length; x++ ) {
+        let difference = temp-sentenceArray[x].length-3;
+        console.log(starRight + sentenceArray[x] + space.repeat(difference) + starLeft);
+    }
+
+    console.log(star); 
+}
+
+frame();
+
+
