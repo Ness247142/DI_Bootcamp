2994c32c7921fc430b975af675c323201f1e320d
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-01-20 09:32:09 +0100

    Update javascript_daily.js

--- Week2/Day2/Daily Challenge/javascript_daily.js
+++ Week2/Day2/Daily Challenge/javascript_daily.js
@@ -1,11 +1,18 @@
 
-let str = "This movie is not bad";
-let not = /not/i;
-let findnot = str.match(not);
-let bad = /bad/i;
-let findbad = str.match(bad);
-console.log(findnot);
-console.log(findbad);
 
-let result = str.replace("not bad", "good");
-console.log(result);
+let string1 = "not";
+let string2 = "bad";
+let testA = "This movie is not so bad";
+let testB = testA.indexOf("not");
+let testC = testA.indexOf("bad");
+console.log(testB);
+console.log(testC);
+
+
+if((testA.includes("not")) && (testA.includes("bad")) && (testA.indexOf("not"))<testA.indexOf("bad")){
+console.log(testA.slice(0, testA.indexOf("not")) + "good");
+}
+
+else {
+console.log(testA);
+}
