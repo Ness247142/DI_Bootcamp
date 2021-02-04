ed0bbe240ddb1f96672af21e4a57a6fcbce84dd0
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-02-04 13:17:43 +0100

    Add files via upload

---
+++ Week4/Day5/box of stars.py
@@ -0,0 +1,20 @@
+def get_longest_word_length(words):
+	longest = 0
+	for word in words:
+		if len(word) > longest:
+			longest = len(word)
+	return longest
+def draw_starline(length):
+	print("*"*(length + 4))
+def draw_wordlines(words, longest):
+	for word in words:
+		spaces = " "*(longest - len(word))
+		print(f"* {word}{spaces} *")
+def run():
+	text = input("Enter a sentence: ")
+	text_list = text.split(" ")
+	longest = get_longest_word_length(text_list)
+	draw_starline(longest)
+	draw_wordlines(text_list, longest)
+	draw_starline(longest)
+run()
