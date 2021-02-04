5a098eee3880c4f2a1ea02d2cd5f8c3ddd0e6241
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-02-04 12:40:09 +0100

    Add files via upload

---
+++ Week4/Day5/Daily Challenge/daily.py
@@ -0,0 +1,15 @@
+# Write a Python function to reverse the bits of an integer (32 bits unsigned).
+# Input : 1234
+# Output : 1260388352
+# For example, 1234 represented in binary as 10011010010 and returns 1260388352 which represents in binary as 01001011001000000000000000000000
+
+# Function to reverse 32 bits
+def reverse_bits(x):
+	bits = 0
+	for i in range(0,32):
+		bits <<= 1
+		bits = bits | x & 1
+		x >>= 1
+	return bits
+            
+print(reverse_bits(1234))
