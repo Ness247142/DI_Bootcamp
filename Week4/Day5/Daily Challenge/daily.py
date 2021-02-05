dc3b7d6ffa8407e2c0e49d353bbe56c769484c4a
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-02-05 11:19:41 +0100

    Update daily.py

--- Week4/Day5/Daily Challenge/daily.py
+++ Week4/Day5/Daily Challenge/daily.py
@@ -3,12 +3,14 @@
 # Output : 1260388352
 # For example, 1234 represented in binary as 10011010010 and returns 1260388352 which represents in binary as 01001011001000000000000000000000
 
+# FUNCTION to reverse the bits of an integer
 def reverse_bits(x):
-	bits = 0
-	for i in range(0,32):
-		bits <<= 1
-		bits = bits | x & 1
-		x >>= 1
-	return bits
-            
+    bits = 0
+    for i in range(0, 32):
+        bits <<= 1
+        bits = bits | x & 1
+        x >>= 1
+    return bits
+
+
 print(reverse_bits(1234))
