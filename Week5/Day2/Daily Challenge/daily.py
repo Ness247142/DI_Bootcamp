d6117e0d724b565bd003c44cc5d2ebd820d29b12
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-02-08 22:47:22 +0100

    Update daily.py

--- Week5/Day2/Daily Challenge/daily.py
+++ Week5/Day2/Daily Challenge/daily.py
@@ -32,46 +32,11 @@ dna1 = DNA("Red-Hair", "XY")
 print(dna1.D_N_A(0))
 
 
-import random
-
-class Gene:
-    def __init__(self, gene):
-        self.gene = gene
-
-    def mutation(self):
-        self.gene = random.randint(0, 1)
-        return self.gene
-
-
-class Chromsome(Gene):
-    def __init__(self, gene):
-        super().__init__(gene)
-        self.chromsome_list = []
-
-    def chromsome_series(self):
-        for x in range(10):
-            self.mutate()
-            self.chromsome_list.append(self.gene)
-        return self.chromsome_list
-
-class DNA(Chromsome):
-    def __init__(self, gene):
-        super().__init__(gene)
-        self.dna_list = []
-
-    def dna_series(self):
-        for i in range(10):
-            self.chromsome_series()
-            self.dna_list.append(self.chromsome_list)
-    
-            
-        print(self.dna_list)
-
-
 class Organism(DNA):
 	def __init__(self, gene, chromosome):
 		super().__init__(gene, chromosome)
 	    self.environment = environment
 
 	def Environment(self, environment):
+
 
