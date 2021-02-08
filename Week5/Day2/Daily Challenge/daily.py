5e52715776bacbe997d6d546d7af03919a004180
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-02-08 22:46:42 +0100

    Update daily.py

--- Week5/Day2/Daily Challenge/daily.py
+++ Week5/Day2/Daily Challenge/daily.py
@@ -4,65 +4,68 @@ class DNA:
 	def __init__(self, gene, chromosome):
 	    self.gene = gene
 	    self.chromosome = chromosome
+	    self.chromosome_list = []
+	    self.dna_list = []
 
 	def Gene(self, gene):
-		gene = [0, 1]
-		return random.randint(0, 1)
+		self.gene = random.randint(0, 1)
+		return self.gene
 
 	def Chromosome(self, chromosome):
-		chromosome = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-		return random.randint(0, 9)
+		for x in range(0, 10):
+		    self.chromosome_list.append(self.gene)
+		return self.chromosome_list
 
 	def D_N_A(self, chromosome):
-		chromosome = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-		return random.randint(0, 9)
+		for x in range(0, 10):
+		    self.dna_list.append(self.chromosome_list)
+		return self.dna_list
 
 
 gene1 = DNA("Blond", "Y")
 print(gene1.Gene(0))
 
 chromo1 = DNA("Brown", "X")
-print(gene1.Chromosome(1))
+print(gene1.Chromosome(0))
 
 dna1 = DNA("Red-Hair", "XY")
-print(dna1.D_N_A(2))
+print(dna1.D_N_A(0))
 
 
+import random
 
-# import random
+class Gene:
+    def __init__(self, gene):
+        self.gene = gene
 
-# class Gene:
-#     def __init__(self, gene):
-#         self.gene = gene
-
-#     def mutation(self):
-#         self.gene = random.randint(0, 1)
-#         return self.gene
+    def mutation(self):
+        self.gene = random.randint(0, 1)
+        return self.gene
 
 
-# class Chromsome(Gene):
-#     def __init__(self, gene):
-#         super().__init__(gene)
-#         self.chromsome_list = []
+class Chromsome(Gene):
+    def __init__(self, gene):
+        super().__init__(gene)
+        self.chromsome_list = []
 
-#     def chromsome_series(self):
-#         for x in range(10):
-#             self.mutate()
-#             self.chromsome_list.append(self.gene)
-#         return self.chromsome_list
+    def chromsome_series(self):
+        for x in range(10):
+            self.mutate()
+            self.chromsome_list.append(self.gene)
+        return self.chromsome_list
 
-# class DNA(Chromsome):
-#     def __init__(self, gene):
-#         super().__init__(gene)
-#         self.dna_list = []
+class DNA(Chromsome):
+    def __init__(self, gene):
+        super().__init__(gene)
+        self.dna_list = []
 
-#     def dna_series(self):
-#         for i in range(10):
-#             self.chromsome_series()
-#             self.dna_list.append(self.chromsome_list)
+    def dna_series(self):
+        for i in range(10):
+            self.chromsome_series()
+            self.dna_list.append(self.chromsome_list)
     
             
-#         print(self.dna_list)
+        print(self.dna_list)
 
 
 class Organism(DNA):
@@ -71,3 +74,4 @@ class Organism(DNA):
 	    self.environment = environment
 
 	def Environment(self, environment):
+
