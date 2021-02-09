2e99765f8391bce07ba228e42da250aac52d8623
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-02-09 09:21:03 +0100

    Update daily.py

--- Week5/Day2/Daily Challenge/daily.py
+++ Week5/Day2/Daily Challenge/daily.py
@@ -1,42 +1,84 @@
-import random
+# import random
+
+# class DNA:
+# 	def __init__(self, gene, chromosome):
+# 	    self.gene = gene
+# 	    self.chromosome = chromosome
+# 	    self.chromosome_list = []
+# 	    self.dna_list = []
+
+# class Gene:
+# 	def __init__(self):
+# 		self.gene = random.randint(0, 1)
+# 		return self.gene
+
+# class Chromosome:
+# 	for x in range(0, 10):
+# 		self.chromosome_list.append(self.gene)
+# 		return self.chromosome_list
+
+# class DNA:
+# 	def __init__(self):
+# 		for x in range(0, 10):
+# 			self.dna_list.append(self.chromosome_list)
+# 			return self.dna_list
+
+
+
+from random import randint, random
+
+class Gene:
+	def __init__(self):
+		self.value = randint(0, 1)
+
+	def mutate(self):
+		self.value == 1 if self.value == 0 else self. value == 1
+		# self.value = int(not bool(self.value))
+
+	def __repr__(self):
+		return str(self.value)
+
+class Chromosome:
+	def __init__(self):
+		self.value = [Gene() for i in range(10)]
+
+	def mutate(self):
+		for gene in self.value:
+			if random() > 0,5:
+				gene.mutate()
+
+	def __repr__(self):
+		return f"{self.value}"
 
 class DNA:
-	def __init__(self, gene, chromosome):
-	    self.gene = gene
-	    self.chromosome = chromosome
-	    self.chromosome_list = []
-	    self.dna_list = []
+	def __init__(self):
+		self.value = [Chromosome()] for _ in range(10)
 
-	def Gene(self, gene):
-		self.gene = random.randint(0, 1)
-		return self.gene
-
-	def Chromosome(self, chromosome):
-		for x in range(0, 10):
-		    self.chromosome_list.append(self.gene)
-		return self.chromosome_list
-
-	def D_N_A(self, chromosome):
-		for x in range(0, 10):
-		    self.dna_list.append(self.chromosome_list)
-		return self.dna_list
+	def __repr__(self):
+		return f"{self.value}"
 
 
-gene1 = DNA("Blond", "Y")
-print(gene1.Gene(0))
 
-chromo1 = DNA("Brown", "X")
-print(gene1.Chromosome(0))
+c = Chromosome()
+c.value()
 
-dna1 = DNA("Red-Hair", "XY")
-print(dna1.D_N_A(0))
+c.mutate()
+
+d = DNA()
+d.value()
+
+
+gene1 = Gene()
+gene1.value()
+
+chromo1 = Chromosome()
+chromo1.value()
+
+dna1 = DNA()
+dna1.value()
 
 
 class Organism(DNA):
-	def __init__(self, gene, chromosome):
-		super().__init__(gene, chromosome)
+	def __init__(self):
+		super().__init__(self)
 	    self.environment = environment
-
-	def Environment(self, environment):
-
-
