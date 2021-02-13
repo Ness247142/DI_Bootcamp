5ad1591db6205d195e62a36d109e4db9ed40068c
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-02-13 17:06:54 +0100

    Update game-final.py

--- Week5/Day5/Mini Project/game-final.py
+++ Week5/Day5/Mini Project/game-final.py
@@ -1,102 +1,78 @@
 import random
 import os
 import time
-import rock-paper-scissors-final
-
-win_count = 0
-lose_count = 0
-tie_count = 0
 
 def clear():
     os.system("clear")
- 
-def game_instructions():
- 
-    print()
-    print("Instructions for Rock-Paper-Scissors : ")
-    print()
-    print("Rock crushes Scissors")
-    print("Scissors cuts Paper")
-    print("Paper covers Rock")
-    print()
- 
- 
-def get_user_item (self):
-     
-    global rps_table
-    global game_map
-    global name
- 
-    # Game Loop for each game of Rock-Paper-Scissors
-    while True:
- 
-        print("--------------------------------------")
-        print("\t\tMenu")
-        print("--------------------------------------")
-        print("Enter \"help\" for instructions")
-        print("Enter \"Rock\",\"Paper\",\"Scissors\" to play")
-        print("Enter \"exit\" to quit")
-        print("--------------------------------------")
- 
-        print()
- 
-        # Player Input
-        inp = input("Enter your move : ")
- 
-        if inp.lower() == "help":
-            clear()
-            game_instructions()
-            continue
-        elif inp.lower() == "exit":
-            clear()
-            break  
-        elif inp.lower() == "rock":
-            player_move = 0
-        elif inp.lower() == "paper":
-            player_move = 1    
-        elif inp.lower() == "scissors":
-            player_move = 2
-        else:
-            clear()
-            print("Wrong Input!!")
-            game_instructions()  
-            continue
- 
-        print("Computer making a move....")
- 
-        print()
-        time.sleep(2)
-    
-    def get_computer_item(self):
-        # Get the computer move randomly
-        comp_move = random.randint(0, 2)
-        # Print the computer move
-        print("Computer chooses ", game_map[comp_move].upper())
- 
-    def get_game_result(self, user_item, computer_item):
-        # Find the winner of the match
-        winner = rps_table[player_move][comp_move]
-        # Declare the winner 
-        if winner == player_move:
-            print(name, "WINS!! THE COMPUTER LOSES!!")
-        elif winner == comp_move:
-            print("THE COMPUTER WINS!! YOU LOSE!!")
-        else:
-            print("IT'S A DRAW!! WANNA PLAIN AGAIN?")
 
-    print()
-    time.sleep(2)
-    clear()
- 
-# The main function
-if __name__ == '__main__':
- 
-    # The mapping between moves and numbers
-    game_map = {0:"rock", 1:"paper", 2:"scissors"}
- 
-    # Win-lose matrix for traditional game
-    rps_table = [[-1, 1, 0], [1, -1, 2], [0, 2, -1]]
- 
-     
-    name = input("Enter your name: ")
- 
+class Game:
+    def __init__(self):
+        self.results = {
+            "win": 0,
+            "loss": 0,
+            "draw": 0
+        }
+
+    
+    def get_user_item(self):
+        user_item = input("Choose (r) for Rock, (p) for Paper or (s) for Scissors: ")
+        while user_item not in ("r", "p", "s"):
+            user_item = input("Choose (r) for Rock, (p) for Paper or (s) for Scissors: ")
+        return user_item
+        time.sleep(2)
+
+
+    def get_computer_item(self):
+        item_list = ["r", "p", "s"]
+        computer_item = random.choice(item_list)
+        return computer_item
+        time.sleep(2)
+
+
+    def get_game_result(self, user_item, computer_item):
+        if user_item == computer_item:
+            print(f"Both you and the computer chose {user_item}. It's a draw!")
+            return "draw"
+        
+        elif user_item == "r":
+            if computer_item == "p":
+                print(f"You chose {user_item} and the computer chose {computer_item}. You lost")
+                return "loss"
+            else:
+                print(f"You chose {user_item} and the computer chose {computer_item}. You won")
+                return "win"
+        
+        elif user_item == "p":
+            if computer_item == "s":
+                print(f"You chose {user_item} and the computer chose {computer_item}. You lost")
+                return "loss"
+            else:
+                print(f"You chose {user_item} and the computer chose {computer_item}. You won")
+                return "win"
+
+        else:
+            if computer_item == "r":
+                print(f"You chose {user_item} and the computer chose {computer_item}. You lost")
+                return "loss"
+            else:
+                print(f"You chose {user_item} and the computer chose {computer_item}. You won")
+                return "win"
+        time.sleep(2)
+        clear()
+
+
+    def play(self):
+
+        user_item = self.get_user_item()
+        computer_item = self.get_computer_item()
+        result = self.get_game_result(user_item, computer_item)
+
+        if result == "win":
+            self.results["win"] += 1
+        elif result == "loss":
+            self.results["loss"] += 1
+        else:
+            self.results["draw"] += 1
+        return self.results
+        time.sleep(2)
+        clear()
