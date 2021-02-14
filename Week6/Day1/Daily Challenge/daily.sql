4d3a28d936259c1056eaa5b30d90e8e19750947e
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-02-14 15:15:31 +0100

    Update daily.sql

--- Week6/Day1/Daily Challenge/daily.sql
+++ Week6/Day1/Daily Challenge/daily.sql
@@ -8,10 +8,8 @@
 
 2
 
-2. Insert into actors
-(first_name, last_name, age, number_oscars)
-values
-('Tom', 'Hanks', 5)
+2. INSERT INTO actors(first_name, last_name, age, number_oscars)
+VALUES('Tom', 'Hanks', 5)
 
 ERROR:  INSERT has more target columns than expressions
 LINE 2: (first_name, last_name, age, number_oscars)
@@ -22,11 +20,9 @@ Character: 44
 -> When I start the query, the message answers me that there is a missing argument at 'age' because we already defined during the creation of the table 'actors', that this field cannot be NULL.
 
 
-Insert into actors
-(first_name, last_name, age, number_oscars)
-values
-('Tom', 'Hanks', '1956-07-09', 5)
+INSERT INTO actors(first_name, last_name, age, number_oscars)
+VALUES('Tom', 'Hanks', '1956-07-09', 5)
 
 1	"Matt"	    "Damon"	    "1970-08-10"	5
-2	" George"	"Clooney"	"1961-06-05"	2
-3	"Tom"	    "Hanks"	    "1956-07-09"	5
+2	"George"	  "Clooney"	  "1961-06-05"	2
+3	"Tom"	      "Hanks"	    "1956-07-09"	5
