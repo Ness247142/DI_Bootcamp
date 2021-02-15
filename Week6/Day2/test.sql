95939760c4dbb0c2f240fc6a6eaf8d2ee17b8cbb
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-02-15 19:27:36 +0100

    Add files via upload

---
+++ Week6/Day2/test.sql
@@ -0,0 +1,18 @@
+
+
+PGCLI
+Pstgres CommandLine Interface
+
+
+
+To switch to Pstgres user:
+sudo -i -u postgres
+password of the computer
+
+
+Primary key = main index of the table / unique identifier for a table
+
+
+Relational Databases
+RDBMS Relational Databases Management System
+
