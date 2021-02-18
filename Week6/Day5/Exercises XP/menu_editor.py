e9e345a19b7d0813cdfcd2e443341e83fad2ed5b
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-02-18 15:46:49 +0100

    Add files via upload

---
+++ Week6/Day5/Exercises XP/menu_editor.py
@@ -0,0 +1,44 @@
+
+from menu_manager import MenuManager
+import sqlite3 as sl
+
+def load_manager():
+	MenuManager()
+	return MenuManager
+
+def show_user_menu():
+	print('    Menu\n (a) Add an item \n (b) Delete item\n (c) View menu \n (d) Exit ')
+	choice = input(' ')
+	if choice == 'a':
+		add_item_to_menu()
+		show_user_menu()
+	elif choice == 'b':
+		remove_item_from_menu()
+		show_user_menu()
+	elif choice == 'c':
+		show_restaurant_menu()
+		show_user_menu()
+	elif choice == 'd':
+		menu2.save_to_file()
+		print(menu2.menu)
+
+def add_item_to_menu():
+	name = input('Name:  ')
+	price = input('Price:  ')
+	menu2.add_item(name, price)
+	print(menu2.menu)
+
+def remove_item_from_menu():
+	name = input("Name of the item:  ")
+	menu2.remove_item(name)
+	print(menu2.menu)
+
+def show_restaurant_menu():
+	for item in menu2.menu['items']:
+		print(item['name'] + ': $' + str(item['price']))
+
+
+menu1 = load_manager()
+menu2 = menu1()
+print(menu2.menu)
+print(show_user_menu())
---
+++ Week6/Day5/Exercises XP/menu_manager.py
@@ -0,0 +1,38 @@
+
+import json
+
+
+class MenuManager:
+	def __init__(self):
+		f = open('menu.txt')
+		self.menu = json.load(f)
+
+	def add_item(self, name, price):
+		x=''
+		for item in self.menu['items']:
+			if name == item['name'] and price == item['price']:
+				x='on'
+				print(f'{name} is already on your menu:')
+				print(self.menu)
+				break
+		if x!= 'on':
+			self.menu['items'].append({'name': name, 'price': price})
+			print(self.menu)
+
+
+	def remove_item(self, name):
+		for item in self.menu['items']:
+			if name == item['name']:
+				self.menu['items'].remove(item)
+		print(self.menu)
+
+	def save_to_file(self):
+		with open('menu.txt', 'w') as f:
+			json.dump(self.menu, f)
+
+
+menu1=MenuManager()
+menu1.add_item('drink', 10)
+menu1.remove_item('drink')
+menu.save_to_file()
+
