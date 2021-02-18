bb7797c26f185e13b39eb8d0928dda6275861237
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-02-18 16:32:32 +0100

    Update menu_manager.py

--- Week6/Day5/Exercises XP/menu_manager.py
+++ Week6/Day5/Exercises XP/menu_manager.py
@@ -1,4 +1,30 @@
 
+{
+    "items": [
+        {
+            "name": "Vegetable soup",
+            "price": 30
+        },
+        {
+            "name": "Hamburger",
+            "price": 44.9
+        },
+        {
+            "name": "Milkshake",
+            "price": 22.5
+        },
+        {
+            "name": "Artichoke",
+            "price": 18
+        },
+        {
+            "name": "Beef stew",
+            "price": 52.5
+        }
+    ]
+}
+
+
 import json
 
 
