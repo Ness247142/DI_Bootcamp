a940a4ebc792ce33c7a7035df8b482d561af2f57
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-02-28 19:44:00 +0100

    Update views.py

--- Week8/Day1/Exercises XP/animal_project/info/views.py
+++ Week8/Day1/Exercises XP/animal_project/info/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render  # this line is added automatically
 from django.http import HttpResponse  # pass view information into the browser
 
+with open('animal_info.json') as fl:
+    data = json.load(fl)
 
-def family(request):
-	return HttpResponse('{"families": [{"id": 1, "name": "Felidae"}, {"id": 2, "name": "Caninae"}, {"id": 3, "name": "Mammal"}, {"id": 4, "name": "Reptile"}, {"id": 5, "name": "Insect"}, {"id": 6, "name": "Arachnid"}, {"id": 7, "name": "Amphibian"}]')
+def family(request, family_id):
+	pass
 
-def animals(request):
-	return HttpResponse('{"animals": [{"id": 1, "name": "Dog", "legs": 4, "weight": 5.67, "height": 4.2, "speed": 34, "family": 2}, {"id": 2, "name": "Domestic Cat", "legs": 4, "weight": 5.67, "height": 4.2, "speed": 34, "family": 1}, {"id": 3, "name": "Panther", "legs": 4, "weight": 5.67, "height": 4.2, "speed": 34, "family": 1}, {"id": 4, "name": "Bears", "legs": 4, "weight": 5.67, "height": 4.2, "speed": 34, "family": 3}, {"id": 5, "name": "Snake", "legs": 0, "weight": 5.67, "height": 4.2, "speed": 34, "family": 4}, {"id": 6, "name": "Ant", "legs": 6, "weight": 5.67, "height": 4.2, "speed": 34, "family": 5}, {"id": 7, "name": "Spider", "legs": 8, "weight": 5.67, "height": 4.2, "speed": 34, "family": 6}, {"id": 8, "name": "Salamander", "legs": 4, "weight": 5.67, "height": 4.2, "speed": 34, "family": 7}]')
+
+def animals(request, animal_id):
+	family_list = data['animals']
+	context = {}
+
+	for list_item in family_list:
+		if animals_id == list_item['id']:
+			context['name'] = list_item['name']
+
+	context['animals'] = []
+	for fam_animal in data['animals']:
+		if fam_animal['family'] == animals_id:
+			context['animals'].append(fam_animal)
+
+	    html = render(request, 'animal.html', context)
+	    return html
