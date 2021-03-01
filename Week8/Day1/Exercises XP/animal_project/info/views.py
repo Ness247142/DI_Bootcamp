a8c6efb25d3ed51cd6f0adbc45bde3186c40070a
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-03-01 09:08:40 +0100

    Update views.py

--- Week8/Day1/Exercises XP/animal_project/info/views.py
+++ Week8/Day1/Exercises XP/animal_project/info/views.py
@@ -32,3 +32,19 @@ def animal(request, animals_id):
 
     html = render(request, 'animal.html', context)
     return html
+
+
+Jonathan's solution
+from django.shortcuts import render
+import json
+def all_animals(request):
+    return render(request, 'animals_list.html', get_animals_data())
+def single_animal(request, id):
+    data = get_animals_data()
+    if id > 0 and id <= len(data['animals']):
+        single_animal = data['animals'][id - 1]
+        return render(request, 'animals_detail.html', {'animal': single_animal})
+def get_animals_data():
+    with open('animals.json', 'r') as f:
+        data = json.load(f)
+    return data
