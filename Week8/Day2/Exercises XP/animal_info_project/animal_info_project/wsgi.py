7664606e44f388fd17acdd4db7645d21b505eb1c
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-03-01 18:55:13 +0100

    Update wsgi.py

--- Week8/Day2/Exercises XP/animal_info_project/animal_info_project/wsgi.py
+++ Week8/Day2/Exercises XP/animal_info_project/animal_info_project/wsgi.py
@@ -1,16 +1,14 @@
 """
-WSGI config for animals project.
-
+WSGI config for animal_info_project project.
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
-https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'animals.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'animal_info_project.settings')
 
 application = get_wsgi_application()
