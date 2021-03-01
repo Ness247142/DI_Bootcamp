72177a73bec19d232c631f876b9c276e3d7e23dd
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-03-01 21:39:41 +0100

    Update admin.py

--- Week8/Day2/Exercises XP/animal_info_project/info/admin.py
+++ Week8/Day2/Exercises XP/animal_info_project/info/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Animal
+from .models import Family 
 
-# Register your models here.
+
+admin.site.register(Animal)
+admin.site.register(Family)
+
