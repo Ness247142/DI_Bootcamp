ecec659439cd1405f9970dd4824c3fc0a273aa67
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-03-01 19:08:31 +0100

    Delete Week8/Day2/Exercises XP/info directory

--- Week8/Day2/Exercises XP/info/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Animal, Family  # import the Animal model
-
-
-# Register your models here.
-admin.site.register(Animal)
-admin.site.register(Family)
--- Week8/Day2/Exercises XP/info/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('/animal/X', views.elon, name='animal')
-    path('/family/X', views.elon, name='family')
-]
