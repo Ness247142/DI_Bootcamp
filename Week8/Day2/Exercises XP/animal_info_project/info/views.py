678e1242b62119ff75d9f0cb99c5def8263112c8
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-03-01 21:41:35 +0100

    Update views.py

--- Week8/Day2/Exercises XP/animal_info_project/info/views.py
+++ Week8/Day2/Exercises XP/animal_info_project/info/views.py
@@ -3,18 +3,20 @@ from .models import Family, Animal
 
 # Create your views here.
 def family(request, family_id):
-    fam = Family.objects.get(id=family_id)
-    animals = Animal.objects.filter(family=fam)
+    family_info = Family.objects.get(id=family_id)
+    animals = Animal.objects.filter(family=family_info)
     context = {
-        'family': fam,
+        'family': family_info,
         'animals': animals
         }
     return render(request, 'family.html', context)
+
 
 def animal(request, animal_id):
     animals = Animal.objects.get(id=animal_id)
     context = {'animal': animal}
     return render(request, 'animal.html', context)
+
 
 def animals(request):
     animals_all = Animal.objects.all()
