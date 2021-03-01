4ea4d9af9b9722c5af2769dc02eaddb58c3c85d7
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-03-01 18:54:43 +0100

    Update urls.py

--- Week8/Day2/Exercises XP/animal_info_project/animal_info_project/urls.py
+++ Week8/Day2/Exercises XP/animal_info_project/animal_info_project/urls.py
@@ -1,7 +1,6 @@
-"""animals URL Configuration
-
+"""animal_info_project URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/dev/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -18,6 +17,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('animal.html')),
-    path('', include('family.html')),
+    path('info/', include('info.urls'))
 ]
