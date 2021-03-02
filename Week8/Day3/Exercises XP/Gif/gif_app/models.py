e53c56284c22442f688798eda9f962deb07ce5e7
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-03-02 19:51:03 +0100

    Update models.py

--- Week8/Day3/Exercises XP/Gif/gif_app/models.py
+++ Week8/Day3/Exercises XP/Gif/gif_app/models.py
@@ -5,14 +5,14 @@ class Gif(models.Model):
     title = models.CharField(max_length=50)
     url = models.URLField(max_length=200)
     uploader_name = models.CharField(max_length=50)
-    created_at = models.DateTimeField(auto_now=False, auto_now_add=False)
+    created_at = models.DateTimeField(auto_now_add=True)
     likes = models.IntegerField()
 
     def __repr__(self):
-        return f"{self.title}"
+        return f"ID, Title: {self.id}, {self.title}"
 
     def __str__(self):
-        return f"{self.title}"
+        return f"ID, Title: {self.id}, {self.title}"
 
 
 class Category(models.Model):
@@ -20,7 +20,7 @@ class Category(models.Model):
     gifs = models.ManyToManyField(Gif, related_name='categories', blank=True)
 
     def __repr__(self):
-        return f"{self.name}"
+        return f"ID, Name: {self.id}, {self.name}"
 
     def __str__(self):
-        return f"{self.name}"
+        return f"ID, Name: {self.id}, {self.name}"
