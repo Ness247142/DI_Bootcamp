bfec7ca20a6e8fa2f91beeb9e4cbdf580e4956e3
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-03-02 19:54:19 +0100

    Update views.py

--- Week8/Day3/Exercises XP/Gif/gif_app/views.py
+++ Week8/Day3/Exercises XP/Gif/gif_app/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
+def home(request):
+    gifs = Gif.objects.all()
+    context = {'gifs': gifs}
+    return render(request, 'home.html', context)
 
-def category(request):
-    return render(request, 'home.html', {'gif_app': Gif.objects.get(id=id)})
-
+def gif_presentation(request, gif_id):
+    gifs = Gif.objects.get(id=gif_id)
+    context = {'gifs': gifs}
+    return render(request, 'gif_presentation.html', context)
 
 def categories(request):
-    return render(request, 'home.html', {'gif_app': Gif.objects.get(id=id)})
-
-
-def gif(request):
-    return render(request, 'home.html', {'gif_app': Gif.objects.get(id=id)})
+    pass
