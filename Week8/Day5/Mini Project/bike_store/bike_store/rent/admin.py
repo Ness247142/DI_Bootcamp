dc55cc0493f14ea17237b9d82f5a812403b267ec
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-03-05 12:36:12 +0100

    Delete Week8/Day5/Mini Project/bike_store/bike_store directory

--- Week8/Day5/Mini Project/bike_store/bike_store/rent/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Customer)
-admin.site.register(Vehicle)
-admin.site.register(VehicleType)
-admin.site.register(VehicleSize)
-admin.site.register(Rental)
-admin.site.register(RentalRate)
