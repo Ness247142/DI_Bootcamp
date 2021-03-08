6d94d54e141c633b34e3ef1a54ce5ca6ba2ffb03
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-03-08 21:21:31 +0100

    Add files via upload

---
+++ Week9/Day2/Daily Challenge/film_project/film_project/account_app/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class SignupForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
---
+++ Week9/Day2/Daily Challenge/film_project/film_project/account_app/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from django.http import Http404
+from cprint import cprint
+from .models import *
+from .forms import *
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password_user = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password_user)
+            login(request, user)
+            return redirect('homepage')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def login(request):
+    if request.method == "GET":
+        my_form = LoginForm()
+        return render(request, 'login.html', {'my_form': my_form})
+
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('homepage')
+
+        else:
+            return redirect('login')
+
+
+def logout(request):
+    logout(request)
+    return redirect('homepage')
+
+
+def profile(request):
+    my_profile = User.objects.get(id=pk)
+    return render(request, 'profile.html', {'my_profile': my_profile})
