7c0ad88e1415e69d030e46d75186dfbdda6fc4df
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-03-10 22:19:37 +0100

    Delete Week9/Day4/Daily Challenge/img_project/img_project directory

--- Week9/Day4/Daily Challenge/img_project/img_project/img_app/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import *
-
-
-class AddImageForm(forms.ModelForm):
-    class Meta:
-        model = Image
-        fields = '__all__'
-        ordering = ('id')
--- Week9/Day4/Daily Challenge/img_project/img_project/img_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('images/', views.images, name='images'),
-]
