4ddfb1fa25bc9985a2f290240e443dd35cd4ffab
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-03-19 20:44:05 +0100

    Update portfolio.js

--- PortfolioProject/portfolio.js
+++ PortfolioProject/portfolio.js
@@ -1,26 +1,30 @@
-
-filterSelection("all")
+filterSelection("all") // Execute the function and show all columns
 function filterSelection(c) {
-  let x, i;
-  x = document.getElementsByClassName("filterDiv");
+  var x, i;
+  x = document.getElementsByClassName("column");
   if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
     w3RemoveClass(x[i], "show");
     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
   }
 }
 
+// Show filtered elements
 function w3AddClass(element, name) {
-  let i, arr1, arr2;
+  var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
   for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) { element.className += " " + arr2[i]; }
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
   }
 }
 
+// Hide elements that are not selected
 function w3RemoveClass(element, name) {
-  let i, arr1, arr2;
+  var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
   for (i = 0; i < arr2.length; i++) {
@@ -31,12 +35,12 @@ function w3RemoveClass(element, name) {
   element.className = arr1.join(" ");
 }
 
-
-let btnContainer = document.getElementById("myBtnContainer");
-let btns = btnContainer.getElementsByClassName("btn");
-for (let i = 0; i < btns.length; i++) {
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
-    let current = document.getElementsByClassName("active");
+    var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
   });
