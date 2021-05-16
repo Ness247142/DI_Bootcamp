7fa67b54784739420d9438a814427cec1dc748b2
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-05-16 18:26:02 +0200

    Update menu_editor.py

--- Week6/Day5/Exercises XP/menu_editor.py
+++ Week6/Day5/Exercises XP/menu_editor.py
@@ -1,9 +1,12 @@
 
+
 from menu_manager import MenuManager
+
 
 def load_manager():
 	MenuManager()
 	return MenuManager
+
 
 def show_user_menu():
 	print('    Menu\n (a) Add an item \n (b) Delete item\n (c) View menu \n (d) Exit ')
@@ -21,17 +24,20 @@ def show_user_menu():
 		menu2.save_to_file()
 		print(menu2.menu)
 
+		
 def add_item_to_menu():
 	name = input('Name:  ')
 	price = input('Price:  ')
 	menu2.add_item(name, price)
 	print(menu2.menu)
 
+	
 def remove_item_from_menu():
 	name = input("Name of the item:  ")
 	menu2.remove_item(name)
 	print(menu2.menu)
 
+	
 def show_restaurant_menu():
 	for item in menu2.menu['items']:
 		print(item['name'] + ': $' + str(item['price']))
@@ -41,3 +47,4 @@ menu1 = load_manager()
 menu2 = menu1()
 print(menu2.menu)
 print(show_user_menu())
+
