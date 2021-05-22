141172002635817d996a5e5d458b0c55a4ff7a60
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-05-22 20:02:11 +0200

    Update urls.py

--- Week8/Day1/Exercises XP/animal_project/info/urls.py
+++ Week8/Day1/Exercises XP/animal_project/info/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('family/<int:families_id>', views.family, name='family'),
-    path('animal/<int:animals_id>', views.animal, name='animal'),
+    path('family/<int:fam_id>', views.family, name='family'),
+    path('animal/<int:anim_id>', views.animal, name='animal')
 ]
