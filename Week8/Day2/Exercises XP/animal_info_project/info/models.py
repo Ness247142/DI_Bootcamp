97d0c100ffbc153f1e31c85d2825b36ba5bc442a
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-05-25 17:24:17 +0200

    Update models.py

--- Week8/Day2/Exercises XP/animal_info_project/info/models.py
+++ Week8/Day2/Exercises XP/animal_info_project/info/models.py
@@ -2,12 +2,20 @@ from django.db import models
 
 # Create your models here.
 class Family(models.Model):
-    name = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.name}'
+
 
 class Animal(models.Model):
-    name = models.CharField(max_length=30, default='')
+    name = models.CharField(max_length=50, default='')
     legs = models.IntegerField(default=0)
     weight = models.FloatField()
     height = models.FloatField()
-    speed = models.FloatField()
+    speed = models.IntegerField()
     family = models.ForeignKey(Family, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}'
+
