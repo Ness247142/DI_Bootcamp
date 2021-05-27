c1942a4f6813c9e958f9aebfc82511259f388aab
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-05-27 14:58:10 +0200

    Delete Week8/Day3/Exercises XP/Gif/gif_app directory

--- Week8/Day3/Exercises XP/Gif/gif_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GifAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gif_app'
--- Week8/Day3/Exercises XP/Gif/gif_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.main, name='main'),
-    path('gif_presentation/<int:gif_id>', views.gif_presentation, name='gif_presentation'),
-    path('', views.categories, name='categories'),
-]
