d0bf9d2cb27cb5dc476ec58cf2fb223e2aff4640
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-05-27 16:55:18 +0200

    Delete Week8/Day4/Exercises XP/todo_project/todo directory

--- Week8/Day4/Exercises XP/todo_project/todo/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('todo/', views.todo, name='todo'),
-    path('display_todos/', views.display_todos, name='display_todos'),
-]
