cde6108ed7f01f1cf0a8ebe912ddd15cefcff557
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-06-01 10:09:09 +0200

    Delete Week9/Day3/Exercises XP/film_project/film_project/account_app directory

--- Week9/Day3/Exercises XP/film_project/film_project/account_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.user.first_name} {self.user.last_name}'
