cc612ad2fc8e07ec3080d5d057d872cde92bc061
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-06-01 10:09:17 +0200

    Delete Week9/Day3/Exercises XP/film_project/film_project/film_app directory

--- Week9/Day3/Exercises XP/film_project/film_project/film_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('homepage', views.homepage, name='homepage'),
-    path('add_film/', views.add_film, name='add_film'),
-    path('add_director/', views.add_director, name='add_director'),
-]
