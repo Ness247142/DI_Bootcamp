5ad5767e1236c7821c0ce1c36a30f3e9d1f28d61
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-11-02 13:15:08 +0200

    Update models.py

--- Week8/Day5/Mini Project/bike_store/rent/models.py
+++ Week8/Day5/Mini Project/bike_store/rent/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
