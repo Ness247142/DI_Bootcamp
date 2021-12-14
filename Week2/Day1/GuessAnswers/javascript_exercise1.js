cd8a60430d8a4ce62e2f84f136648ef13caa182b
Ness247142 <64212283+Ness247142@users.noreply.github.com>
2021-12-14 17:09:27 +0200

    Update javascript_exercise1.js

--- Week2/Day1/GuessAnswers/javascript_exercise1.js
+++ Week2/Day1/GuessAnswers/javascript_exercise1.js
@@ -2,6 +2,7 @@
     5 - "4" // 1
     10 % 5 // 0
     5 % 10 // 5
+
     "Java" + "Script" // "JavaScript"
     " " + " " // "  "
     " " + 0 // " 0"
@@ -10,4 +11,5 @@
     false + true // 1
     false - true // -1
     3 - 4 // -1
+
     "Bob" - "bill" // NaN
